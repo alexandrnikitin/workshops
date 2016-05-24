4137145bee27f82009e4d6dc6e86b3a6a5918b12
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-05-24 16:43:32 +0300

    SandboxApp for profiling

---
+++ high-performance-dotnet-by-example/code/AhoCorasickTree/Adform.AdServing.AhoCorasickTree.SandboxApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adform.AdServing.AhoCorasickTree.SandboxApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new AhoCorasickTree(new[] { "ab", "abc", "bcd" });
+
+
+            for (var i = 0; i < 30000000; i++)
+            {
+                tree.Contains("bcd");
+            }
+
+        }
+    }
+}
