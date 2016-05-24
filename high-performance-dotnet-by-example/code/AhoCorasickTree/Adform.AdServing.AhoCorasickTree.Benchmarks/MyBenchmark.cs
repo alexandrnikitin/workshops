4434389eeecf5c61e666ef6d697614c3d0c23976
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-05-24 16:14:45 +0300

    Benchmarks proj

---
+++ high-performance-dotnet-by-example/code/AhoCorasickTree/Adform.AdServing.AhoCorasickTree.Benchmarks/MyBenchmark.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+
+
+namespace Adform.AdServing.AhoCorasickTree.Benchmarks
+{
+    [Config(typeof(Config))]
+    public class MyBenchmark
+    {
+        private AhoCorasickTree _tree;
+
+        public MyBenchmark()
+        {
+            _tree = new AhoCorasickTree(new[] {"ab", "abc", "bcd"});
+        }
+
+        [Benchmark]
+        public bool Test()
+        {
+            return _tree.Contains("ab");
+        }
+
+        private class Config : ManualConfig
+        {
+            public Config()
+            {
+                Add(new Job {LaunchCount = 1, WarmupCount = 5, TargetCount = 5});
+            }
+        }
+    }
+}
