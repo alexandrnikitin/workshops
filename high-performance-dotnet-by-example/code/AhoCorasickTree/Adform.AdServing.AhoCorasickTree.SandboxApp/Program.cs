e69aa7c73396c03607225bab489370232bd4a867
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-05-24 17:10:40 +0300

    New version used

--- high-performance-dotnet-by-example/code/AhoCorasickTree/Adform.AdServing.AhoCorasickTree.SandboxApp/Program.cs
+++ high-performance-dotnet-by-example/code/AhoCorasickTree/Adform.AdServing.AhoCorasickTree.SandboxApp/Program.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using AhoCorasickTreeV2 = Adform.AdServing.AhoCorasickTree.Sandbox.V2.AhoCorasickTree;
+
 namespace Adform.AdServing.AhoCorasickTree.SandboxApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var tree = new AhoCorasickTree(new[] { "ab", "abc", "bcd" });
+            var tree = new AhoCorasickTreeV2(new[] { "ab", "abc", "bcd" });
 
 
             for (var i = 0; i < 30000000; i++)
