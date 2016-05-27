f34e0c7b5622ccdfde23750369c35921a9ae6345
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-05-27 17:42:31 +0300

    Benches

--- high-performance-dotnet-by-example/code/AhoCorasickTree/Adform.AdServing.AhoCorasickTree.Benchmarks/ManyKeywordsBenchmark.cs
+++ high-performance-dotnet-by-example/code/AhoCorasickTree/Adform.AdServing.AhoCorasickTree.Benchmarks/ManyKeywordsBenchmark.cs
@@ -10,6 +10,8 @@ using AhoCorasickTreeV4 = Adform.AdServing.AhoCorasickTree.Sandbox.V4.AhoCorasic
 using AhoCorasickTreeV5 = Adform.AdServing.AhoCorasickTree.Sandbox.V5.AhoCorasickTree;
 using AhoCorasickTreeV6 = Adform.AdServing.AhoCorasickTree.Sandbox.V6.AhoCorasickTree;
 using AhoCorasickTreeV7 = Adform.AdServing.AhoCorasickTree.Sandbox.V7.AhoCorasickTree;
+using AhoCorasickTreeV8 = Adform.AdServing.AhoCorasickTree.Sandbox.V8.AhoCorasickTree;
+using AhoCorasickTreeV9 = Adform.AdServing.AhoCorasickTree.Sandbox.V9.AhoCorasickTree;
 
 namespace Adform.AdServing.AhoCorasickTree.Benchmarks
 {
@@ -19,12 +21,14 @@ namespace Adform.AdServing.AhoCorasickTree.Benchmarks
         private const string UserAgent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36";
         private readonly AhoCorasickTree _tree;
         private readonly AhoCorasickTreeV2 _tree2;
-        private readonly AhoCorasickTreeV3 _tree2a;
-        private readonly AhoCorasickTreeV3a _tree3;
+        private readonly AhoCorasickTreeV3 _tree3;
+        private readonly AhoCorasickTreeV3a _tree3a;
         private readonly AhoCorasickTreeV4 _tree4;
         private readonly AhoCorasickTreeV5 _tree5;
         private readonly AhoCorasickTreeV6 _tree6;
         private readonly AhoCorasickTreeV7 _tree7;
+        private readonly AhoCorasickTreeV8 _tree8;
+        private readonly AhoCorasickTreeV9 _tree9;
 
         public ManyKeywordsBenchmark()
         {
@@ -32,12 +36,14 @@ namespace Adform.AdServing.AhoCorasickTree.Benchmarks
 
             _tree = new AhoCorasickTree(keywords);
             _tree2 = new AhoCorasickTreeV2(keywords);
-            _tree2a = new AhoCorasickTreeV3(keywords);
-            _tree3 = new AhoCorasickTreeV3a(keywords);
+            _tree3 = new AhoCorasickTreeV3(keywords);
+            _tree3a = new AhoCorasickTreeV3a(keywords);
             _tree4 = new AhoCorasickTreeV4(keywords);
             _tree5 = new AhoCorasickTreeV5(keywords);
             _tree6 = new AhoCorasickTreeV6(keywords);
             _tree7 = new AhoCorasickTreeV7(keywords);
+            _tree8 = new AhoCorasickTreeV8(keywords);
+            _tree9 = new AhoCorasickTreeV9(keywords);
         }
 
         [Benchmark(Baseline = true)]
@@ -53,15 +59,15 @@ namespace Adform.AdServing.AhoCorasickTree.Benchmarks
         }
 
         [Benchmark]
-        public bool Test2a()
-        {
-            return _tree2a.Contains(UserAgent);
-        }
-
-        [Benchmark]
         public bool Test3()
         {
             return _tree3.Contains(UserAgent);
+        }
+
+        [Benchmark]
+        public bool Test3a()
+        {
+            return _tree3a.Contains(UserAgent);
         }
 
         [Benchmark]
@@ -86,6 +92,17 @@ namespace Adform.AdServing.AhoCorasickTree.Benchmarks
         public bool Test7()
         {
             return _tree7.Contains(UserAgent);
+        }
+        [Benchmark]
+        public bool Test8()
+        {
+            return _tree8.Contains(UserAgent);
+        }
+
+        [Benchmark]
+        public bool Test9()
+        {
+            return _tree9.Contains(UserAgent);
         }
 
         private class Config : ManualConfig
